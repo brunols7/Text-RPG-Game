93da8402c6a14e1188f177435d506ae8e29edb5a
Bruno Leonardo <135679874+brunols7@users.noreply.github.com>
2024-04-19 20:31:58 -0300

    Update jogo.java

--- src/main/java/jogo.java
+++ src/main/java/jogo.java
@@ -2,6 +2,12 @@ import java.util.Scanner;
 
 public class jogo {
 
-  // Jogo vai se passar aqui
-  
+  public static void main(String[] args){
+    
+      Personagem personagem = criarPersonagem();
+      int vida = personagem.vida;
+      int agilidade = personagem.agilidade;
+      double dano = personagem.dano;
+    
+  }
 }
