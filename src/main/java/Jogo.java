fb277338fe936999a4d3981cc8acdc89dbc23775
Bruno Leonardo <135679874+brunols7@users.noreply.github.com>
2024-04-26 19:41:23 -0300

    Update Jogo.java

--- src/main/java/Jogo.java
+++ src/main/java/Jogo.java
@@ -110,4 +110,9 @@ public class Jogo {
         //double dano = personagem.dano;
         
     }
+
+    public static void clearConsole() {
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();  
+    }
 }
