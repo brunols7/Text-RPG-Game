209aad7cff5806a30381c3b099522873d66c4aa0
Bruno Leonardo <bruno.lsilva1508@gmail.com>
2024-04-28 16:08:54 -0300

    comments

--- src/main/java/Jogo.java
+++ src/main/java/Jogo.java
@@ -1,54 +1,5 @@
 import java.util.Scanner;
  
-// Classe usada para criar o personagem
-class Personagem {
-  int vida;
-  int agilidade;
-  double dano;
- 
-  // Classe usada para usuário escolher qual personagem ele vai querer jogar, podendo escolher pelos pontos de dano, vida e agilidade.
-  public static Personagem criarPersonagem() throws InterruptedException {
-      Scanner sc = new Scanner(System.in);
-      Personagem personagem = new Personagem();
-      int opcao;
- 
-      do {
-          System.out.println("ESCOLHA UM PERSONAGEM:\n ");
-          System.out.println("[1] Jonn\n\nVida: 100\nAgilidade: 60\nDano: 8%\n");
-          System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
-          System.out.println("[2] Anne\n\nVida: 100\nAgilidade: 80\nDano: 6%\n");
- 
-          System.out.print("OPÇÃO DESEJADA: ");
-          opcao = sc.nextInt();
-          System.out.println("");
- 
-          if (opcao == 1) {
-              System.out.println("PERSONAGEM ESCOLHIDO: \u001B[36mJONN\u001B[0m\n");
-              personagem.vida = 100;
-              personagem.agilidade = 60;
-              personagem.dano = 0.8;
-          } else if (opcao == 2) {
-            // Colorindo a String no Console utilizando \u001B[36m e outros.
-            System.out.println("PERSONAGEM ESCOLHIDO: \u001B[36mANNE\u001B[0m\n");
-            personagem.vida = 100;
-            personagem.agilidade = 80;
-            personagem.dano = 0.6;
-          } else {
-              System.out.println("\nOPÇÃO INVÁLIDA!\n");
-          }
-          Thread.sleep(2500);
-          System.out.println("\n=-=-=-=-=-= INICIANDO JOGO =-=-=-=-=-=");
-          Thread.sleep(2000);
-          System.out.println("\n\n\n\n\n");
-      } while (opcao < 1 || opcao > 2);
- 
-      sc.close();
- 
-      return personagem;
-  }
- 
-}
- 
 public class Jogo {
    
     // Função principal onde é o Menu e onde vai chamar as outras funções que o jogo possue.
@@ -69,20 +20,24 @@ public class Jogo {
             opcaoMenu = sc.nextInt();
  
             switch (opcaoMenu) {
+                // se escolher jogo
                 case 1:
                     clearConsole();
                     System.out.println("\n=-=-=-=-=-= INICIANDO JOGO =-=-=-=-=-=");
                     Thread.sleep(1000);
-                    Personagem personagem = Personagem.criarPersonagem();
+                    Personagem personagem = criarPersonagem();
                     jogar(personagem);
                     opcaoMenu = 4;
                     break;
+                //se escolher instrução
                 case 2:
                     instrucoes();
                     break;
+                // se escolher créditos
                 case 3:
                     printCreditos();
                     break;
+                // se escolher sair
                 case 4:
                     System.out.println("\nSaindo do Jogo. Volte Logo!");
                     System.exit(0);
@@ -96,7 +51,56 @@ public class Jogo {
  
         sc.close();
     }
- 
+    
+    // Classe de personagem que define a vida, agilidade e dano do personagem.
+    static class Personagem {
+        int vida;
+        int agilidade;
+        double dano;
+    }
+
+    // Função usada para usuário escolher qual personagem ele vai querer jogar, podendo escolher pelos pontos de dano, vida e agilidade.
+    public static Personagem criarPersonagem() throws InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        Personagem personagem = new Personagem();
+        int opcao;
+
+        do {
+            System.out.println("ESCOLHA UM PERSONAGEM:\n ");
+            System.out.println("[1] Jonn\n\nVida: 100\nAgilidade: 60\nDano: 8%\n");
+            System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+            System.out.println("[2] Anne\n\nVida: 100\nAgilidade: 80\nDano: 6%\n");
+
+            System.out.print("OPÇÃO DESEJADA: ");
+            opcao = sc.nextInt();
+            System.out.println("");
+
+            if (opcao == 1) {
+                // Colorindo a String no Console
+                System.out.println("PERSONAGEM ESCOLHIDO: \u001B[36mJONN\u001B[0m\n");
+                personagem.vida = 100;
+                personagem.agilidade = 60;
+                personagem.dano = 0.8;
+            } else if (opcao == 2) {
+                // Colorindo a String no Console utilizando \u001B[36m e outros.
+                System.out.println("PERSONAGEM ESCOLHIDO: \u001B[36mANNE\u001B[0m\n");
+                personagem.vida = 100;
+                personagem.agilidade = 80;
+                personagem.dano = 0.6;
+            } else {
+                System.out.println("\nOPÇÃO INVÁLIDA!\n");
+            }
+            Thread.sleep(2500);
+            System.out.println("\n=-=-=-=-=-= INICIANDO JOGO =-=-=-=-=-=");
+            Thread.sleep(2000);
+            System.out.println("\n\n\n\n\n");
+        } while (opcao < 1 || opcao > 2);
+
+        sc.close();
+
+        return personagem;
+    }
+
     // Função onde mostra os créditos, utilizamos uma String com texto em ASCII.
     public static void printCreditos() {
         String creditos = " _____    _ _                                                                       \n" +
@@ -126,12 +130,18 @@ public class Jogo {
     public static void jogar(Personagem personagem) throws InterruptedException{
  
         Scanner input = new Scanner(System.in);
- 
+
+        // Variáveis que vão ser usadas para o personagem escolhido, utilizando os valores de vida, agilidade e dano escolhidos.
         int vida = personagem.vida;
         int agilidade = personagem.agilidade;
         double dano = personagem.dano;
        
         clearConsole();
+
+        /*
+         * Variáveis de textos em ASCII para a história do jogo.
+         */
+
         String historiaString =
                 " ██░ ██  ██▓  ██████ ▄▄▄█████▓ ▒█████   ██▀███   ██▓ ▄▄▄      \n" +
                 "▓██░ ██▒▓██▒▒██    ▒ ▓  ██▒ ▓▒▒██▒  ██▒▓██ ▒ ██▒▓██▒▒████▄    \n" +
@@ -155,10 +165,10 @@ public class Jogo {
  
         System.out.println("\u001B[31m" + historiaString + "\u001B[0m");
         System.out.println("Após a perda recente de seu pai, um mestre da matemática, você se encontra em um estado de luto e confusão. Em uma noite, você decide tentar entrar em contato com ele, na esperança de sentir sua presença de alguma forma. ");
-        Thread.sleep(1000);
+        Thread.sleep(4000);
         System.out.println("Porém você acaba encontrando um outro ser.");
         System.out.println("\n\n");
-        Thread.sleep(10000);
+        Thread.sleep(8000);
         System.out.println("\u001B[31m " + boaSorteString + "\u001B[0m");
         Thread.sleep(5000);
  
